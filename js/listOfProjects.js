624631588263c9de43054434aaf72065cd17b38d
Daniel <dks2151@columbia.edu>
2025-03-25 23:24:33 -0400

    Reworked website
    Converted to bootstrap based on stuff learned in UI class.
    
    Updated photos.
    Reworked CSS classes to reduce the mess

--- js/listOfProjects.js
+++ js/listOfProjects.js
@@ -1,6 +1,6 @@
 import { projects } from "../data.js";
 
-const projectGrid = document.querySelector(".project-grid");
+const projectGrid = document.querySelector("#project-gallery");
 
 const saveProjectData = (data) => {
   localStorage.setItem("project-data", JSON.stringify(data));
@@ -10,14 +10,14 @@ const saveProjectData = (data) => {
   const populateProjects = (projects) => {
 
     projects.forEach((project, i) =>{
-      const projectCard =   `<div class='project'  data-index=${i}>
-                                <h3> ${project.name} </h3>
+      const projectCard =   `<h3> ${project.name} </h3>
                                 <figure>
                                         <img class="icon" src=${project.photo} alt="${project.name}">
                                 </figure>
-                                <a class="link-button" href='./project-page.html' > Learn More </a>
-                            </div>`
+                                <a class="nav-button" href='./project-page.html' > Learn More </a>`
             const div = document.createElement('div');
+            div.classList.add('col-md-4')
+            div.setAttribute('data-index', i)
             div.innerHTML = projectCard
             div.addEventListener('click', () => saveProjectData(project))
             projectGrid.append(div)
@@ -26,3 +26,4 @@ const saveProjectData = (data) => {
   }
 
   populateProjects(projects)
+
